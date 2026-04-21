1dcf10a32820bc296bc3eea4ff26624fdb261ab3
Praise Tobby <156103404+PgTobby-07@users.noreply.github.com>
2026-04-21 04:27:01 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,8 +7,9 @@ app = FastAPI(title="Servecly API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "https://servecly.vercel.app" // Add your actual Vercel URL here
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173",
+        "https://servecly.vercel.app/" # Add your Vercel URL here!
     ],
     allow_credentials=True,
     allow_methods=["*"],
