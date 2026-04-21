ee6ab1634b75fc80ec626b51fa6bed4a8e2bc086
Praise Tobby <156103404+PgTobby-07@users.noreply.github.com>
2026-04-21 07:29:26 +0300

    Update DATABASE_URL to remove SSL certificate
    Removed SSL certificate path from DATABASE_URL.

--- backend/config/database.py
+++ backend/config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://274Zmr7wnstVpWK.root:1W2Wy2S029sQ1yRy@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/test?ssl_ca=/etc/ssl/certs/ca-certificates.crt"
+DATABASE_URL = "mysql://274Zmr7wnstVpWK.root:1W2Wy2SO29sQ1yRy@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/test"
 
 engine = create_engine(DATABASE_URL)
 
