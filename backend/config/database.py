f0aefa0c66927103a81af47db41facd7d0bc0582
Praise Tobby <156103404+PgTobby-07@users.noreply.github.com>
2026-04-21 05:10:27 +0300

    Update DATABASE_URL for AWS TiDB connection

--- backend/config/database.py
+++ backend/config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/servecly"
+DATABASE_URL = "mysql+pymysql://274Zmr7wnstVpWK.root:1W2Wy2S029sQ1yRy@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/test?ssl_ca=/etc/ssl/certs/ca-certificates.crt"
 
 engine = create_engine(DATABASE_URL)
 
