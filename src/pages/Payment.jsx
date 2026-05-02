716897dba9b11bb821f10066ad61433a67131b1b
Praise Tobby <156103404+PgTobby-07@users.noreply.github.com>
2026-05-02 22:50:05 +0300

    Add Link import to Payment component

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import MainLayout from '../layouts/MainLayout';
+import { Link } from 'react-router-dom';
 
 const Payment = () => {
   const [method, setMethod] = useState('card');
