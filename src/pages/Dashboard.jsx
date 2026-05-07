bf11ea0f25a520b3bc3d035725c320b74efde956
Praise Tobby <156103404+PgTobby-07@users.noreply.github.com>
2026-05-07 19:04:16 +0300

    Fix typo in useNavigate hook initialization

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import {Link, useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  const navigate = usedgdjfdjjfkjfjfhjfNavigate();
+  const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [liveData, setLiveData] = useState([]);
   const [loading, setLoading] = useState(true);
